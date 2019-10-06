6e22ef3b9aba978d67163a1f17bdf3c093aebf76
DonggunJung <donggun.dev@gmail.com>
2019-10-06 15:58:34 +0900

    Apply react apollo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-console.log('hi')
+import * as express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
+
+const typeDefs = gql`
+	type Book {
+		title: String
+		author: String
+	}
+
+	type Query {
+		books: [Book]
+	}
+`
+
+const books = [
+	{
+		title: 'Harry Potter and the Chamber of Secrets',
+		author: 'J.K. Rowling'
+	},
+	{
+		title: 'Jurassic Park',
+		author: 'Michael Crichton'
+	}
+]
+
+const resolvers = {
+	Query: {
+		books: () => books
+	}
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+const app = express()
+
+server.applyMiddleware({ app })
+
+app.listen({ port: 4000 }, () =>
+	console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+)
