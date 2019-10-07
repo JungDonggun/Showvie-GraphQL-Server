5e0e9e0621dc113832b66593cbd7209dba59a22f
DonggunJung <donggun.dev@gmail.com>
2019-10-07 15:33:54 +0900

    Add Prisma Movie

--- prisma/generated/prisma-client/prisma-schema.ts
+++ prisma/generated/prisma-client/prisma-schema.ts
@@ -2,7 +2,11 @@
   // Please don't change this file manually but run `prisma generate` to update it.
   // For more information, please read the docs: https://www.prisma.io/docs/prisma-client/
 
-export const typeDefs = /* GraphQL */ `type AggregateUser {
+export const typeDefs = /* GraphQL */ `type AggregateMovieReview {
+  count: Int!
+}
+
+type AggregateUser {
   count: Int!
 }
 
@@ -10,9 +14,291 @@ type BatchPayload {
   count: Long!
 }
 
+scalar DateTime
+
 scalar Long
 
+type MovieReview {
+  id: ID!
+  title: String!
+  rating: Int!
+  review: String
+  createdAt: DateTime!
+  updatedAt: DateTime!
+}
+
+type MovieReviewConnection {
+  pageInfo: PageInfo!
+  edges: [MovieReviewEdge]!
+  aggregate: AggregateMovieReview!
+}
+
+input MovieReviewCreateInput {
+  id: ID
+  title: String!
+  rating: Int
+  review: String
+}
+
+input MovieReviewCreateManyInput {
+  create: [MovieReviewCreateInput!]
+  connect: [MovieReviewWhereUniqueInput!]
+}
+
+type MovieReviewEdge {
+  node: MovieReview!
+  cursor: String!
+}
+
+enum MovieReviewOrderByInput {
+  id_ASC
+  id_DESC
+  title_ASC
+  title_DESC
+  rating_ASC
+  rating_DESC
+  review_ASC
+  review_DESC
+  createdAt_ASC
+  createdAt_DESC
+  updatedAt_ASC
+  updatedAt_DESC
+}
+
+type MovieReviewPreviousValues {
+  id: ID!
+  title: String!
+  rating: Int!
+  review: String
+  createdAt: DateTime!
+  updatedAt: DateTime!
+}
+
+input MovieReviewScalarWhereInput {
+  id: ID
+  id_not: ID
+  id_in: [ID!]
+  id_not_in: [ID!]
+  id_lt: ID
+  id_lte: ID
+  id_gt: ID
+  id_gte: ID
+  id_contains: ID
+  id_not_contains: ID
+  id_starts_with: ID
+  id_not_starts_with: ID
+  id_ends_with: ID
+  id_not_ends_with: ID
+  title: String
+  title_not: String
+  title_in: [String!]
+  title_not_in: [String!]
+  title_lt: String
+  title_lte: String
+  title_gt: String
+  title_gte: String
+  title_contains: String
+  title_not_contains: String
+  title_starts_with: String
+  title_not_starts_with: String
+  title_ends_with: String
+  title_not_ends_with: String
+  rating: Int
+  rating_not: Int
+  rating_in: [Int!]
+  rating_not_in: [Int!]
+  rating_lt: Int
+  rating_lte: Int
+  rating_gt: Int
+  rating_gte: Int
+  review: String
+  review_not: String
+  review_in: [String!]
+  review_not_in: [String!]
+  review_lt: String
+  review_lte: String
+  review_gt: String
+  review_gte: String
+  review_contains: String
+  review_not_contains: String
+  review_starts_with: String
+  review_not_starts_with: String
+  review_ends_with: String
+  review_not_ends_with: String
+  createdAt: DateTime
+  createdAt_not: DateTime
+  createdAt_in: [DateTime!]
+  createdAt_not_in: [DateTime!]
+  createdAt_lt: DateTime
+  createdAt_lte: DateTime
+  createdAt_gt: DateTime
+  createdAt_gte: DateTime
+  updatedAt: DateTime
+  updatedAt_not: DateTime
+  updatedAt_in: [DateTime!]
+  updatedAt_not_in: [DateTime!]
+  updatedAt_lt: DateTime
+  updatedAt_lte: DateTime
+  updatedAt_gt: DateTime
+  updatedAt_gte: DateTime
+  AND: [MovieReviewScalarWhereInput!]
+  OR: [MovieReviewScalarWhereInput!]
+  NOT: [MovieReviewScalarWhereInput!]
+}
+
+type MovieReviewSubscriptionPayload {
+  mutation: MutationType!
+  node: MovieReview
+  updatedFields: [String!]
+  previousValues: MovieReviewPreviousValues
+}
+
+input MovieReviewSubscriptionWhereInput {
+  mutation_in: [MutationType!]
+  updatedFields_contains: String
+  updatedFields_contains_every: [String!]
+  updatedFields_contains_some: [String!]
+  node: MovieReviewWhereInput
+  AND: [MovieReviewSubscriptionWhereInput!]
+  OR: [MovieReviewSubscriptionWhereInput!]
+  NOT: [MovieReviewSubscriptionWhereInput!]
+}
+
+input MovieReviewUpdateDataInput {
+  title: String
+  rating: Int
+  review: String
+}
+
+input MovieReviewUpdateInput {
+  title: String
+  rating: Int
+  review: String
+}
+
+input MovieReviewUpdateManyDataInput {
+  title: String
+  rating: Int
+  review: String
+}
+
+input MovieReviewUpdateManyInput {
+  create: [MovieReviewCreateInput!]
+  update: [MovieReviewUpdateWithWhereUniqueNestedInput!]
+  upsert: [MovieReviewUpsertWithWhereUniqueNestedInput!]
+  delete: [MovieReviewWhereUniqueInput!]
+  connect: [MovieReviewWhereUniqueInput!]
+  set: [MovieReviewWhereUniqueInput!]
+  disconnect: [MovieReviewWhereUniqueInput!]
+  deleteMany: [MovieReviewScalarWhereInput!]
+  updateMany: [MovieReviewUpdateManyWithWhereNestedInput!]
+}
+
+input MovieReviewUpdateManyMutationInput {
+  title: String
+  rating: Int
+  review: String
+}
+
+input MovieReviewUpdateManyWithWhereNestedInput {
+  where: MovieReviewScalarWhereInput!
+  data: MovieReviewUpdateManyDataInput!
+}
+
+input MovieReviewUpdateWithWhereUniqueNestedInput {
+  where: MovieReviewWhereUniqueInput!
+  data: MovieReviewUpdateDataInput!
+}
+
+input MovieReviewUpsertWithWhereUniqueNestedInput {
+  where: MovieReviewWhereUniqueInput!
+  update: MovieReviewUpdateDataInput!
+  create: MovieReviewCreateInput!
+}
+
+input MovieReviewWhereInput {
+  id: ID
+  id_not: ID
+  id_in: [ID!]
+  id_not_in: [ID!]
+  id_lt: ID
+  id_lte: ID
+  id_gt: ID
+  id_gte: ID
+  id_contains: ID
+  id_not_contains: ID
+  id_starts_with: ID
+  id_not_starts_with: ID
+  id_ends_with: ID
+  id_not_ends_with: ID
+  title: String
+  title_not: String
+  title_in: [String!]
+  title_not_in: [String!]
+  title_lt: String
+  title_lte: String
+  title_gt: String
+  title_gte: String
+  title_contains: String
+  title_not_contains: String
+  title_starts_with: String
+  title_not_starts_with: String
+  title_ends_with: String
+  title_not_ends_with: String
+  rating: Int
+  rating_not: Int
+  rating_in: [Int!]
+  rating_not_in: [Int!]
+  rating_lt: Int
+  rating_lte: Int
+  rating_gt: Int
+  rating_gte: Int
+  review: String
+  review_not: String
+  review_in: [String!]
+  review_not_in: [String!]
+  review_lt: String
+  review_lte: String
+  review_gt: String
+  review_gte: String
+  review_contains: String
+  review_not_contains: String
+  review_starts_with: String
+  review_not_starts_with: String
+  review_ends_with: String
+  review_not_ends_with: String
+  createdAt: DateTime
+  createdAt_not: DateTime
+  createdAt_in: [DateTime!]
+  createdAt_not_in: [DateTime!]
+  createdAt_lt: DateTime
+  createdAt_lte: DateTime
+  createdAt_gt: DateTime
+  createdAt_gte: DateTime
+  updatedAt: DateTime
+  updatedAt_not: DateTime
+  updatedAt_in: [DateTime!]
+  updatedAt_not_in: [DateTime!]
+  updatedAt_lt: DateTime
+  updatedAt_lte: DateTime
+  updatedAt_gt: DateTime
+  updatedAt_gte: DateTime
+  AND: [MovieReviewWhereInput!]
+  OR: [MovieReviewWhereInput!]
+  NOT: [MovieReviewWhereInput!]
+}
+
+input MovieReviewWhereUniqueInput {
+  id: ID
+}
+
 type Mutation {
+  createMovieReview(data: MovieReviewCreateInput!): MovieReview!
+  updateMovieReview(data: MovieReviewUpdateInput!, where: MovieReviewWhereUniqueInput!): MovieReview
+  updateManyMovieReviews(data: MovieReviewUpdateManyMutationInput!, where: MovieReviewWhereInput): BatchPayload!
+  upsertMovieReview(where: MovieReviewWhereUniqueInput!, create: MovieReviewCreateInput!, update: MovieReviewUpdateInput!): MovieReview!
+  deleteMovieReview(where: MovieReviewWhereUniqueInput!): MovieReview
+  deleteManyMovieReviews(where: MovieReviewWhereInput): BatchPayload!
   createUser(data: UserCreateInput!): User!
   updateUser(data: UserUpdateInput!, where: UserWhereUniqueInput!): User
   updateManyUsers(data: UserUpdateManyMutationInput!, where: UserWhereInput): BatchPayload!
@@ -39,6 +325,9 @@ type PageInfo {
 }
 
 type Query {
+  movieReview(where: MovieReviewWhereUniqueInput!): MovieReview
+  movieReviews(where: MovieReviewWhereInput, orderBy: MovieReviewOrderByInput, skip: Int, after: String, before: String, first: Int, last: Int): [MovieReview]!
+  movieReviewsConnection(where: MovieReviewWhereInput, orderBy: MovieReviewOrderByInput, skip: Int, after: String, before: String, first: Int, last: Int): MovieReviewConnection!
   user(where: UserWhereUniqueInput!): User
   users(where: UserWhereInput, orderBy: UserOrderByInput, skip: Int, after: String, before: String, first: Int, last: Int): [User]!
   usersConnection(where: UserWhereInput, orderBy: UserOrderByInput, skip: Int, after: String, before: String, first: Int, last: Int): UserConnection!
@@ -46,13 +335,18 @@ type Query {
 }
 
 type Subscription {
+  movieReview(where: MovieReviewSubscriptionWhereInput): MovieReviewSubscriptionPayload
   user(where: UserSubscriptionWhereInput): UserSubscriptionPayload
 }
 
 type User {
   id: ID!
+  identity: String!
   name: String!
   password: String!
+  createdAt: DateTime!
+  updatedAt: DateTime!
+  movieReview(where: MovieReviewWhereInput, orderBy: MovieReviewOrderByInput, skip: Int, after: String, before: String, first: Int, last: Int): [MovieReview!]
 }
 
 type UserConnection {
@@ -63,8 +357,10 @@ type UserConnection {
 
 input UserCreateInput {
   id: ID
+  identity: String!
   name: String!
   password: String!
+  movieReview: MovieReviewCreateManyInput
 }
 
 type UserEdge {
@@ -75,16 +371,25 @@ type UserEdge {
 enum UserOrderByInput {
   id_ASC
   id_DESC
+  identity_ASC
+  identity_DESC
   name_ASC
   name_DESC
   password_ASC
   password_DESC
+  createdAt_ASC
+  createdAt_DESC
+  updatedAt_ASC
+  updatedAt_DESC
 }
 
 type UserPreviousValues {
   id: ID!
+  identity: String!
   name: String!
   password: String!
+  createdAt: DateTime!
+  updatedAt: DateTime!
 }
 
 type UserSubscriptionPayload {
@@ -106,11 +411,14 @@ input UserSubscriptionWhereInput {
 }
 
 input UserUpdateInput {
+  identity: String
   name: String
   password: String
+  movieReview: MovieReviewUpdateManyInput
 }
 
 input UserUpdateManyMutationInput {
+  identity: String
   name: String
   password: String
 }
@@ -130,6 +438,20 @@ input UserWhereInput {
   id_not_starts_with: ID
   id_ends_with: ID
   id_not_ends_with: ID
+  identity: String
+  identity_not: String
+  identity_in: [String!]
+  identity_not_in: [String!]
+  identity_lt: String
+  identity_lte: String
+  identity_gt: String
+  identity_gte: String
+  identity_contains: String
+  identity_not_contains: String
+  identity_starts_with: String
+  identity_not_starts_with: String
+  identity_ends_with: String
+  identity_not_ends_with: String
   name: String
   name_not: String
   name_in: [String!]
@@ -158,6 +480,25 @@ input UserWhereInput {
   password_not_starts_with: String
   password_ends_with: String
   password_not_ends_with: String
+  createdAt: DateTime
+  createdAt_not: DateTime
+  createdAt_in: [DateTime!]
+  createdAt_not_in: [DateTime!]
+  createdAt_lt: DateTime
+  createdAt_lte: DateTime
+  createdAt_gt: DateTime
+  createdAt_gte: DateTime
+  updatedAt: DateTime
+  updatedAt_not: DateTime
+  updatedAt_in: [DateTime!]
+  updatedAt_not_in: [DateTime!]
+  updatedAt_lt: DateTime
+  updatedAt_lte: DateTime
+  updatedAt_gt: DateTime
+  updatedAt_gte: DateTime
+  movieReview_every: MovieReviewWhereInput
+  movieReview_some: MovieReviewWhereInput
+  movieReview_none: MovieReviewWhereInput
   AND: [UserWhereInput!]
   OR: [UserWhereInput!]
   NOT: [UserWhereInput!]
@@ -165,6 +506,6 @@ input UserWhereInput {
 
 input UserWhereUniqueInput {
   id: ID
-  name: String
+  identity: String
 }
 `
