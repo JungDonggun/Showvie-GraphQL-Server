f41b56c4434515ca16745c1c8e59ee7319a54c9d
정동건 <donggun.dev@gmail.com>
2019-10-20 15:41:50 +0900

    Add movie type

---
+++ lib/types/movie.ts
@@ -0,0 +1,11 @@
+export type movieType = {
+	title: string
+	alternativeTitle: string
+	extent: string
+	language: string
+	regDate: string
+	person: string
+	referenceIdentifier: string
+	rights: string
+	subjectCategory: string
+}
