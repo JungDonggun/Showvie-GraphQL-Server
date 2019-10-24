68041f441c78ccab4f9cbbb121c2eebe61f57809
정동건 <donggun.dev@gmail.com>
2019-10-24 17:23:35 +0900

    Update return data

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -5,16 +5,15 @@ import { get } from 'config'
 // Resolver란, query에서 특정 필드에 대한 요청이 있을 때, 그것을 어떤 로직으로 처리할지 GraphQL에게 알려주는 역할을 합니다.
 const resolvers = {
 	Query: {
-		movieList: async (parent, { subjectCategory, offset, limit }, context, info) => {
+		movieList: async (parent, { subjectCategory, limit }, context, info) => {
 			console.log('Start at movieList', {
 				subjectCategory,
-				offset,
 				limit
 			})
 
-			const movieList = await context.prisma.movieLists({
+			const getMovieList = await context.prisma.movieLists({
 				orderBy: 'regDate_ASC',
-				skip: offset,
+				skip: limit - 10,
 				last: limit,
 				where: {
 					referenceIdentifier_not: '',
@@ -22,11 +21,7 @@ const resolvers = {
 				}
 			})
 
-			return {
-				offset,
-				limit,
-				movieList
-			}
+			return getMovieList
 		}
 	},
 	Mutation: {
