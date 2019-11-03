a2a14e1badddf73efee0e77d3d015bbad2989d60
정동건 <donggun.dev@gmail.com>
2019-11-03 20:32:12 +0900

    Update seed data in seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,9 @@
-let prisma = {}
+import { prisma } from './generated/prisma-client'
 
 async function main() {
 	await prisma.createUser({
-		identity: 'admin',
-		name: '어드민',
+		email: 'admin',
+		nickname: '어드민',
 		password: '7060182FE1BB70D4074D1FA6E260A2F55A280751A7B27B608FD5F35A2EAAEB05',
 		movieReviews: {
 			create: {
@@ -13,6 +13,10 @@ async function main() {
 			}
 		}
 	})
+
+	await prisma.createmovieTag({
+		tags: '음악,로맨스,코미디,호러,액션,판타지,SF,애니메이션,스포츠,뮤지컬,추리,스릴러'
+	})
 }
 
-main()
+export default main()
